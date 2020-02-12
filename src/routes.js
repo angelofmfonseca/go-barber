08f1dd64a1fe2c11948efadbc9d2861ca21bda47
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-12 13:13:50 -0300

    routes e controller dos appointments

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import multer from "multer";
-import multerConfig from "./config/multer";
 
+import multerConfig from "./config/multer";
+import authMiddleware from "./app/middlewares/auth";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
-import authMiddleware from "./app/middlewares/auth";
+import AppointmentController from "./app/controllers/AppointmentController";
 
 const routes = new Router();
 
@@ -14,12 +15,12 @@ const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+routes.post("/files", upload.single("file"), FileController.store);
+routes.post("/appointments", AppointmentController.store);
 
 routes.use(authMiddleware);
 routes.put("/users", UserController.update);
 
 routes.get("/providers", ProviderController.index);
-
-routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
