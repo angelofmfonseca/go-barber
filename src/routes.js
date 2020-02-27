26efc871b2f0fd54bb7524fbe3b31326a96f37e3
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-26 22:10:09 -0300

    listando notificações e marcando as lidas

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
 
 const routes = new Router();
 
@@ -21,9 +22,11 @@ routes.post("/appointments", AppointmentController.store);
 
 routes.use(authMiddleware);
 routes.put("/users", UserController.update);
+routes.put("/notifications/:id", NotificationController.update);
 
 routes.get("/providers", ProviderController.index);
 routes.get("/appointments", AppointmentController.index);
 routes.get("/schedule", ScheduleController.index);
+routes.get("/notifications", NotificationController.index);
 
 export default routes;
