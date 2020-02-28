6ad388876fb29c6cad947f395182d63133a3682e
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-27 22:24:23 -0300

    nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import Appointment from "../models/Appointment";
 import User from "../models/User";
 import File from "../models/File";
 import Notification from "../schemas/Notification";
+import mail from "../../lib/Mail";
+import Mail from "../../lib/Mail";
 
 class AppointmentController {
   async index(req, res) {
@@ -110,7 +112,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.paramns.id);
+    const appointment = await Appointment.findByPk(req.paramns.id, {
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["name", "email"]
+        }
+      ]
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -129,6 +139,12 @@ class AppointmentController {
     appointment.canceled_at = new date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: "Appointment cancelled",
+      text: "You have a cancelled appointment."
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: "2525",
+  secure: false,
+  auth: {
+    user: "f2b632c89afdce",
+    pass: "617663b90682dd"
+  },
+  dafult: {
+    from: "Equipe GoBarber <noreply@gobarber.com>"
+  }
+};
