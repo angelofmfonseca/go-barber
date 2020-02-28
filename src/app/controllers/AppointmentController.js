7da9b2f0647e18a4cb7651aa38f61d7bedf896da
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-27 23:12:15 -0300

    template de e-mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -118,6 +118,11 @@ class AppointmentController {
           model: User,
           as: "provider",
           attributes: ["name", "email"]
+        },
+        {
+          model: User,
+          as: "user",
+          attributes: ["name"]
         }
       ]
     });
@@ -143,7 +148,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: "Appointment cancelled",
-      text: "You have a cancelled appointment."
+      template: "cancellation",
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt
+        })
+      }
     });
 
     return res.json(appointment);
