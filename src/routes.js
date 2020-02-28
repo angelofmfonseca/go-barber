7c6a944fdd344372c4271677e466a1a36d5e116a
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-02-27 21:28:34 -0300

    cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -16,17 +16,22 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
-routes.post("/sessions", SessionController.store);
-routes.post("/files", upload.single("file"), FileController.store);
-routes.post("/appointments", AppointmentController.store);
-
 routes.use(authMiddleware);
 routes.put("/users", UserController.update);
+
+routes.post("/files", upload.single("file"), FileController.store);
+
+routes.post("/sessions", SessionController.store);
+
+routes.get("/appointments", AppointmentController.index);
+routes.post("/appointments", AppointmentController.store);
+routes.delete("appointments/:id", AppointmentController.delete);
+
 routes.put("/notifications/:id", NotificationController.update);
+routes.get("/notifications", NotificationController.index);
 
 routes.get("/providers", ProviderController.index);
-routes.get("/appointments", AppointmentController.index);
+
 routes.get("/schedule", ScheduleController.index);
-routes.get("/notifications", NotificationController.index);
 
 export default routes;
