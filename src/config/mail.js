0a6bb2ebdc6abdfb81e6eec27d2c93fbbf4711da
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-03-04 21:48:26 -0300

    .env

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: "smtp.mailtrap.io",
-  port: "2525",
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: "f2b632c89afdce",
-    pass: "617663b90682dd"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   dafult: {
     from: "Equipe GoBarber <noreply@gobarber.com>"
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
-export default = {
-  dsn: "https://5da37c858f7c47f0941bdd3c6eed2ef7@sentry.io/3672677"
-}
+export default {
+  dsn: process.env.SENTRY_DSN
+};
